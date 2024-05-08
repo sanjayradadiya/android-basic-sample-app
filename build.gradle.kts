296d57fecc0d1f19d371799e17ab938ff2c56052
sanjay radadiya <sanjay.radadiya91@gmail.com>
2024-05-08 17:50:57 +0530

    added ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.2" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0" apply true
 }
+
+buildscript {
+    repositories {
+        maven("https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:11.1.0")
+    }
+}
+
+apply(plugin = "org.jlleitschuh.gradle.ktlint")
